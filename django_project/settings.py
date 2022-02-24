d5f285e169e0ce35162476734e2e6b7c3239a1a9
Somesh Bhandare <someshbhandare3@gmail.com>
2022-02-24 16:25:11 +0530

    second commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '##Enter your secrete key##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
